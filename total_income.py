ae4e6b455c32ee3a0577b3e53aad1c4fdb1eb738
Tore <tore.hansen@my.jcu.edu.au>
2017-08-21 13:10:28 +1000

    rename month variable to number, refactor the report printing into its own function

--- total_income.py
+++ total_income.py
@@ -9,16 +9,20 @@ def main():
     incomes = []
     months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for number in range(1, months + 1):
+        income = float(input("Enter income for month {}: ".format(number)))
         incomes.append(income)
 
+    print_income_report(incomes, months)
+
+
+def print_income_report(incomes, months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
+    for number in range(1, months + 1):
+        income = incomes[number - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(number, income, total))
 
 
 main()
