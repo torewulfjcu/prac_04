7f6a33798611ffc34efe12e8a911768e6350d479
Tore <tore.hansen@my.jcu.edu.au>
2017-08-21 13:54:44 +1000

    add program that checks if a username is in a list of usernames

---
+++ security_checker.py
@@ -0,0 +1,9 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Please enter username: ")
+if username in usernames:
+    print("Access granted.")
+else:
+    print("Access denied.")
