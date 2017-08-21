e5d558b6b05c6293e4d0eb4030c5f3b072e2f455
Tore <tore.hansen@my.jcu.edu.au>
2017-08-21 13:46:56 +1000

    add program that utilizes different functions and methods on list

---
+++ list_functions.py
@@ -0,0 +1,16 @@
+from statistics import mean
+
+numbers = []
+print("Please enter 5 numbers.")
+for number in range(0, 5):
+    numbers.append(int(input("Number: ")))
+
+print("The first number is: {}".format(numbers[0]))
+print("The last number is: {}".format(numbers[4]))
+print("The smallest number is: {}".format(min(numbers)))
+print("The largest number is: {}".format(max(numbers)))
+print("The average of the numbers is: {}".format((mean(numbers))))
+
+
+
+
