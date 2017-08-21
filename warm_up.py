18aa356f95a2fc031a2276dca21dc321abdbf631
Tore <tore.hansen@my.jcu.edu.au>
2017-08-21 12:39:34 +1000

    add warm up exercises on lists

---
+++ warm_up.py
@@ -0,0 +1,15 @@
+"""Answers"""
+
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers [3] = 1
+# numbers[:-1] = every element until [-1] which is 2
+# numbers[3:4] = 1
+# 5 in numbers = true
+# 7 in numbers = false
+# "3" in numbers = false
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
