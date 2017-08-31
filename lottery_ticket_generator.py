d16fefc5cf655259ccb9d62817d7f015cc24e3e7
Tore <tore.hansen@my.jcu.edu.au>
2017-08-31 11:22:24 +1000

    add program for generating a lottery ticket consisting of a user defined number of picks - without duplicates and printed in ascending order

--- lottery_ticket_generator.py
+++ lottery_ticket_generator.py
@@ -10,11 +10,19 @@ for line in range(0, number_of_quick_picks):
         lottery_ticket[line].append(random.randint(1, 45))
 
 for line in range(0, number_of_quick_picks):
-    lottery_ticket[line] = set(lottery_ticket[line])
-    lottery_ticket[line] = list(lottery_ticket[line])
+    lottery_ticket[line] = list(set(lottery_ticket[line]))
     while len(lottery_ticket[line]) is not LENGTH_OF_LINE:
         lottery_ticket[line].append(random.randint(1, 45))
-        lottery_ticket[line] = set(lottery_ticket[line])
-        lottery_ticket[line] = list(lottery_ticket[line])
-    print((sorted(lottery_ticket[line])))
+        lottery_ticket[line] = list(set(lottery_ticket[line]))
 
+
+#for line in range(0, number_of_quick_picks):
+#    print(''.join(str(sorted(lottery_ticket[line]))))
+
+for line in range (0, number_of_quick_picks):
+    lottery_ticket.sort()
+    print(" ".join("{:2}".format(number) for line in lottery_ticket for number in line))
+
+
+# s = ' '.join([str(number) for line in lottery_ticket for number in line])
+# print(s)
