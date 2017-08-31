4214b6181bc23b43e070ad912881f6e6bd8c8f2d
Tore <tore.hansen@my.jcu.edu.au>
2017-08-31 11:22:08 +1000

    add program that checks if a username is in a list of usernames

--- list_functions.py
+++ list_functions.py
@@ -10,7 +10,3 @@ print("The last number is: {}".format(numbers[4]))
 print("The smallest number is: {}".format(min(numbers)))
 print("The largest number is: {}".format(max(numbers)))
 print("The average of the numbers is: {}".format((mean(numbers))))
-
-
-
-
